94a9259d28bdcfcc5834b64b994a7edf0a716ad8
Maximiliano Lira Del Canto <8040628+mliradelc@users.noreply.github.com>
2022-04-01 00:34:02 +0200

    Changed draft to a more likely to work formula

--- src/plugins/org.opencast.analytics.matomoUserTrackingDataPlugin.js
+++ src/plugins/org.opencast.analytics.matomoUserTrackingDataPlugin.js
@@ -1,246 +1,115 @@
+/**
+ * Licensed to The Apereo Foundation under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ *
+ * The Apereo Foundation licenses this file to you under the Educational
+ * Community License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License
+ * at:
+ *
+ *   http://opensource.org/licenses/ecl2.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+ import { DataPlugin, Events } from 'paella-core';
 
-import { DataPlugin  } from "paella-core";
-
-export default class MatomoAnalyticsUserTrackingDataPlugin extends DataPlugin {
-    async load() {
-        const siteId = this.config.siteId;
-        const server = this.config.server || "auto";
-        const heartBeat = this.config.heartBeat;
-        const privacyUrl = this.config.privacyUrl;
-        const trackingClient = this.config.trackingClientName;
-        const askForConcent = this.config.askForConcent;
-        const cookieConsentBaseColor = this.config.cookieConsentBaseColor;
-        const cookieConsentHighlightColor = this.config.cookieConsentHighlightColor;
-        
-        var trackingPermission,
-            tracked;
-
-            cookieconsent_base_color = this.cookieconsent_base_color;
-            cookieconsent_highlight_color = this.cookieconsent_highlight_color;
-  
-        function trackMatomo() {
-        if (isTrackingPermission() && !tracked && server && site_id){
-            if (server.substr(-1) != '/') server += '/';
-            paella.require(server + tracking_client + ".js")
-            .then((matomo) => {
-                paella.log.debug("Matomo Analytics Enabled");
-                paella.userTracking.matomotracker = Matomo.getAsyncTracker( server + tracking_client + ".php", site_id );
-                paella.userTracking.matomotracker.client_id = thisClass.config.client_id;
-                if (heartbeat && heartbeat > 0) paella.userTracking.matomotracker.enableHeartBeatTimer(heartbeat);
-                if (Matomo && Matomo.MediaAnalytics) {
-                paella.events.bind(paella.events.videoReady, () => {
-                    Matomo.MediaAnalytics.scanForMedia();
-                });
-                }
-                thisClass.registerVisit();
-                tracked = true;
-            });
-            return true;
-        }	else {
-            paella.log.debug("No tracking permission or no Matomo Site ID found in config file. Disabling Matomo Analytics PlugIn");
-            return false;
-        }
-        }
-
-        function initCookieNotification() {
-            // load cookieconsent lib from remote server
-            paella.require(paella.baseUrl + 'javascript/cookieconsent.min.js')
-            .then(() => {
-                paella.log.debug("Matomo: cookie consent lib loaded");
-                window.cookieconsent.initialise({
-                    "palette": {
-                        "popup": {
-                            "background": cookieconsent_base_color
-                        },
-                        "button": {
-                            "background": cookieconsent_highlight_color
-                        }
-                    },
-                    "type": "opt-in",
-                    "position": "top",
-                    "content": {
-                        "message": translate('matomo_message', "This site would like to use Matomo to collect usage information of the recordings."),
-                        "allow": translate('matomo_accept', "Accept"),
-                        "deny": translate('matomo_deny', "Deny"),
-                        "link": translate('matomo_more_info', "More information"),
-                        "policy": translate('matomo_policy', "Cookie Policy"),
-                        // link to the Matomo platform privacy policy - describing what are we collecting
-                        // through the platform
-                        "href": privacy_url
-                    },
-                    onInitialise: function (status) {
-                        var type = this.options.type;
-                        var didConsent = this.hasConsented();
-                        // enable cookies - send user data to the platform
-                        // only if the user enabled cookies
-                        if (type == 'opt-in' && didConsent) {
-                            setTrackingPermission(true);
-                        } else {
-                            setTrackingPermission(false);
-                        }
-                    },
-                    onStatusChange: function (status, chosenBefore) {
-                        var type = this.options.type;
-                        var didConsent = this.hasConsented();
-                        // enable cookies - send user data to the platform
-                        // only if the user enabled cookies
-                        setTrackingPermission(type == 'opt-in' && didConsent);
-                    },
-                    onRevokeChoice: function () {
-                        var type = this.options.type;
-                        var didConsent = this.hasConsented();
-                        // disable cookies - set what to do when
-                        // the user revokes cookie usage
-                        setTrackingPermission(type == 'opt-in' && didConsent);
-                    }
-                })
-            });
-        }
-
-        function initTranslate(language, funcSuccess, funcError) {
-            paella.log.debug('Matomo: selecting language ' + language.slice(0,2));
-            var jsonstr = window.location.origin + '/player/localization/paella_' + language.slice(0,2) + '.json';
-            $.ajax({
-                url: jsonstr,
-                dataType: 'json',
-                success: function (data) {
-                    if (data) {
-                        data.value_locale = language;
-                        translations = data;
-                        if (funcSuccess) {
-                            funcSuccess(translations);
-                        }
-                    } else if (funcError) {
-                        funcError();
-                    }
-                },
-                error: function () {
-                    if (funcError) {
-                        funcError();
-                    }
-                }
-            });
-        }
-
-        function translate(str, strIfNotFound) {
-            return (translations[str] != undefined) ? translations[str] : strIfNotFound;
-        }
-
-        function isTrackingPermission() {
-            if (checkDoNotTrackStatus() || !trackingPermission) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-
-        function checkDoNotTrackStatus() {
-            if (window.navigator.doNotTrack == 1 || window.navigator.msDoNotTrack == 1) {
-                paella.log.debug("Matomo: Browser DoNotTrack: true");
-                return true;
-            }
-            paella.log.debug("Matomo: Browser DoNotTrack: false");
-            return false;
-        }
-
-        function setTrackingPermission(permissionStatus) {
-            trackingPermission = permissionStatus;
-            paella.log.debug("Matomo: trackingPermissions: " + permissionStatus);
-            trackMatomo();
-        };
-
-        if (ask_for_concent) {
-        initTranslate(navigator.language, function () {
-            paella.log.debug('Matomo: Successfully translated.');
-            initCookieNotification();
-        }, function () {
-            paella.log.debug('Matomo: Error translating.');
-            initCookieNotification();
-        });
-        } else {
-        trackingPermission = true;
-        }
-
-        onSuccess(trackMatomo());
-
-    } // checkEnabled
-
-    setVideoTitleAttr(){
-        var video_element = video_element = document.getElementsByTagName("video");
-        video_element.video_0.setAttribute("data-matomo-title", document.title);
-    }
-
-    registerVisit() {
-        var title,
-            event_id,
-            series_title,
-            series_id,
-            presenter,
-            view_mode;
-
-        if ((paella.opencast != undefined) && (paella.opencast._episode != undefined)) {
-        title = paella.opencast._episode.dcTitle;
-        event_id = paella.opencast._episode.id;
-        presenter = paella.opencast._episode.dcCreator;
-        paella.userTracking.matomotracker.setCustomVariable(5, "client",
-            (paella.userTracking.matomotracker.client_id || "Paella Opencast"));
-        } else {
-        title = this.loadTitle();
-        // Add title for Matomo Media Analytics.
-        if (this.config.html_title){
-            this.setVideoTitleAttr();
-        } else{
-            Matomo.MediaAnalytics.setMediaTitleFallback(function (mediaElement) {return title; });
-        }
-        paella.userTracking.matomotracker.setCustomVariable(5, "client",
-            (paella.userTracking.matomotracker.client_id || "Paella Standalone"));          
-        }
-
-        if ((paella.opencast != undefined) && (paella.opencast._episode != undefined) && (paella.opencast._episode.mediapackage != undefined)) {
-        series_id = paella.opencast._episode.mediapackage.series;
-        series_title = paella.opencast._episode.mediapackage.seriestitle;
-        }
-
-        if (title)
-        paella.userTracking.matomotracker.setCustomVariable(1, "event", title + " (" + event_id + ")", "page");
-        if (series_title)
-        paella.userTracking.matomotracker.setCustomVariable(2, "series", series_title + " (" + series_id + ")", "page");
-        if (presenter) paella.userTracking.matomotracker.setCustomVariable(3, "presenter", presenter, "page");
-        paella.userTracking.matomotracker.setCustomVariable(4, "view_mode", view_mode, "page");
-        if (title && presenter) {
-        paella.userTracking.matomotracker.setDocumentTitle(title + " - " + (presenter || "Unknown"));
-        paella.userTracking.matomotracker.trackPageView(title + " - " + (presenter || "Unknown"));
-        } else {
-        paella.userTracking.matomotracker.trackPageView();
-        }
-    }
-
-    loadTitle() {
-        var title = paella.player.videoLoader.getMetadata() && paella.player.videoLoader.getMetadata().title;
-        return title;
-    }
-
-
-    
-    async write(context, { id }, data) {
-        if (this.config.category === undefined || this.config.category === true) {
-            const category = this.config.category || "PaellaPlayer";
-            const action = data.event;
-            const labelData = {
-                videoId: id,
-                plugin: data.plugin
-            }
-
-            try {
-                // Test if data parameters can be serialized
-                JSON.stringify(data.params);
-                labelData.params = data.params;
-            }
-            catch(e) {}
-
-            const label = JSON.stringify(labelData);
-
-            __gaTracker(' send', 'event', category, action, label);
-        }
-    }
-}
+ export default class OpencastMatomoTrackingDataPlugin extends DataPlugin {
+ 
+   async isEnabled() {
+     try {
+       const response = await fetch('/usertracking/detailenabled');
+       const data = await response.text();
+       const enabled = /true/i.test(data);
+       return enabled;
+     }
+     catch(e) {
+       return false;
+     }
+   }
+ 
+   async load(){
+     var server = this.config.server;
+     const tracking_client = this.config.tracking_client;
+     const site_id = this.config.site_id;
+ 
+     const loadExternalScript = (path) => {
+       return new Promise((resolve) => {
+         const script = document.createElement('script');
+         script.type = 'text/javascript';
+         script.src = path;
+         let loaded = false;
+         script.onload = script.onreadystatechange = () => {
+           if (!loaded) {
+             loaded = true;
+             resolve();
+           }
+         };
+         document.head.appendChild(script);
+       });
+     };
+ 
+     if (server.substr(-1) != '/') server += '/';
+     loadExternalScript(server + tracking_client + '.js').then((matomo) => {
+       this.player.log.debug('Matomo Analytics Enabled');
+       this.player.matomotracker = matomo.getAsyncTracker( server + tracking_client + '.php', site_id );
+     });
+ 
+   }
+ 
+   async write(context, { id }, data) {
+     const currentTime = await this.player.videoContainer.currentTime();
+     const playing = !(await this.player.videoContainer.paused());
+     this.player.log.debug(`Logging event for video id ${ id } at time: ${ currentTime }`);
+     this.player.log.debug(JSON.stringify(this.player.videoManifest));
+     this.player.log.debug(`Video title: ${ this.player.videoManifest.metadata.title }`);
+     const opencastLog = {
+       id,
+       type: data.event.substr(0,128),
+       in: Math.round(currentTime),
+       out: Math.round(currentTime),
+       playing
+     };
+ 
+     switch (data.event) {
+     case Events.PLAY:
+       opencastLog.type = 'PLAY';
+       break;
+     case Events.PAUSE:
+       opencastLog.type = 'PAUSE';
+       break;
+     case Events.SEEK:
+       opencastLog.type = 'SEEK';
+       opencastLog.in = Math.round(data.params.prevTime);
+       break;
+     case Events.FULLSCREEN_CHANGED:
+ 
+       break;
+     case Events.RESIZE_END:
+       opencastLog.type = `RESIZE-TO-${ data.params.size.w }x${ data.params.size.h }`;
+       break;
+     case Events.BUTTON_PRESS:
+       opencastLog.type += '-' + data.plugin;
+       break;
+     default:
+       opencastLog.type += params;
+     }
+ 
+     const params = (new URLSearchParams(opencastLog)).toString();
+     const requestUrl = `/usertracking/?_method=PUT&${ params }`;
+     const result = await fetch(requestUrl);
+     if (!result.ok) {
+       this.player.log.error('Error in user data log');
+     }
+     else {
+       this.player.log.debug(`Opencast user log event done: '${ opencastLog.type }'`);
+     }
+   }
+ }
+ 
